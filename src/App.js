cece8981413eda42e10c85238fe7f17a9a8ad1e4
Ryan Brandle <r.brandle@gmail.com>
2025-09-05 14:42:46 -0600

    started stubbing game

--- src/App.js
+++ src/App.js
@@ -1,3 +1,38 @@
-export default function Square() {
-  return <button className="square">X</button>;
-}
+import { useState } from 'react';
+
+const Square = () => {
+	const [value, setValue] = useState(null);
+
+	const handleClick = () => {
+		setValue('X');
+	};
+
+	return (
+		<button className='square' onClick={handleClick}>
+			{value}
+		</button>
+	);
+};
+
+const Board = () => {
+	return (
+		<>
+			<div className='board-row'>
+				<Square />
+				<Square />
+				<Square />
+			</div>
+			<div className='board-row'>
+				<Square />
+				<Square />
+				<Square />
+			</div>
+			<div className='board-row'>
+				<Square />
+				<Square />
+				<Square />
+			</div>
+		</>
+	);
+};
+export default Board;
