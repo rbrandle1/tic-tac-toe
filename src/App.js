e90ecfa4751278720a6687897ffe0d59d3ba4faf
Ryan Brandle <r.brandle@gmail.com>
2025-09-08 15:59:06 -0600

    updated timeline button to text accordingly based on current move

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useState } from 'react';
  * If you have extra time or want to practice your new React skills, here are some ideas for improvements that you could make to the tic-tac-toe game, listed in order of increasing difficulty:
 
 For the current move only, show “You are at move #…” instead of a button.
+
 Rewrite Board to use two loops to make the squares instead of hardcoding them.
 Add a toggle button that lets you sort the moves in either ascending or descending order.
 When someone wins, highlight the three squares that caused the win (and when no one wins, display a message about the result being a draw).
@@ -113,7 +114,11 @@ const Game = () => {
 
 		return (
 			<li key={move}>
-				<button onClick={() => jumpTo(move)}>{description}</button>
+				{move === currentMove ? (
+					<span>You are at move {currentMove}</span>
+				) : (
+					<button onClick={() => jumpTo(move)}>{description}</button>
+				)}
 			</li>
 		);
 	});
