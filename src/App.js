b827efb82b0bfc03b3a57a98d4b7699d9ef1c39c
Ryan Brandle <r.brandle@gmail.com>
2025-09-08 15:34:32 -0600

    added calculateWinner function tip

--- src/App.js
+++ src/App.js
@@ -106,6 +106,36 @@ Display the location for each move in the format (row, col) in the move history 
  * 
  */
 
+// JS func to calculate a winner.
+// squares is a nine string array. This will check if any specific combinations of the values in this array match any winning value and will declare a winner.
+const calculateWinner = (squares) => {
+	// These identify the winning square combinations. 0,1,2 is the top line.
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	// This for loop will loop through the lines and check for a winning combination.
+	// So if [0,1,2] then a=0, b=1, c=2;
+	// Standard "for loop", Start with i = 0. Keep going as long as i is smaller than the number of things in lines. After each round, add 1 to i.
+	for (let i = 0; i < lines.length; i++) {
+		// This is destructuring. It is shorthand way to pull out the value of the first, second and third value in a line with the the right index. The for loop will check each of the line arrays and run it through the check below. If any of them are the same then you have a match.
+		//This line is extracting each value of each line. So let's pull out 0, 1 and 2. So now a=0, b=1 and c=2.
+		const [a, b, c] = lines[i];
+		// Is there something in the first spot (not empty)? And is the same thing in the second spot? and is the same thing in the third spot? (if all 3 are the same then we have a winner)
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			// return the winning value which will either be X or O.
+			return squares[a];
+		}
+	}
+	return null;
+};
+
 const Square = () => {
 	return <button className='square'>x</button>;
 };
